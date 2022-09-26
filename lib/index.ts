f5ac46f54cab43763d16bea9bdde3e633ad9be07
lyancoder <liangyan9308@163.com>
2022-09-26 14:44:40 +0800

    export custom class and types

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,6 @@ export * from "./constants/common.type";
 export * from "./constants/fiscal.type";
 export * from "./constants/fprinter.type";
 export * from "./printer/epson/xml-http";
+export * from "./printer/custom/xml-custom-http";
+export * from "./constants/custom/custom.type";
+export * from "./constants/custom/fprinter.custom";
